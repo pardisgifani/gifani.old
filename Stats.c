7266ee7482c2b7a6f465919b6eb85755c61c7785
pardisgifani <64362199+pardisgifani@users.noreply.github.com>
2021-04-30 12:50:02 -0700

    Update Stats.c

--- Stats.c
+++ Stats.c
@@ -1,15 +1,22 @@
 /******************************************************************************
  @file <stats.c> 
  @author <Pardis Gifani>
+ @date 05/28/2021
+
+   * Instructions:
  
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
+        This is the 1st HW of the Coursera Course Embedded Software.
+ 
+                main() - The main entry point for your program
+                print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+                print_array() - Given an array of data and a length, prints the array to the screen             [DONE]
+                find_median() - Given an array of data and a length, returns the median value                   [DONE]
+                find_mean() - Given an array of data and a length, returns the mean                             [DONE]
+                find_maximum() - Given an array of data and a length, returns the maximum                       [DONE]
+                find_minimum() - Given an array of data and a length, returns the minimum                       [DONE]
+                sort_array() - Given an array of data and a length, sorts the array from largest to smallest.   [DONE]
+                        (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+
  *****************************************************************************/
 
 #include <stdio.h>
@@ -110,7 +117,8 @@ int find_minimum(unsigned char array[]) {
 
 /* sort_array() - Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value */
 void sort_array(unsigned char array[], unsigned int length) {
-	unsigned char tmp; // selection sort
+	int i = 0, j = 0;
+	unsigned char tmp = 0; // selection sort
 	for(int i = 0; i < length; i++){
 		for(int j = i; j < length; j++){
 			if(array[j] < array[i]){
