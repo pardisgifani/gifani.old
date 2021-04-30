6a601090cf387c87346c7ed988a5c42de581766d
pardisgifani <64362199+pardisgifani@users.noreply.github.com>
2021-04-30 12:33:24 -0700

    Create Stats.h

---
+++ Stats.h
@@ -0,0 +1,14 @@
+
+   * Instructions:
+ 
+        This is the 1st HW of the Coursera Course Embedded Software.
+ 
+                main() - The main entry point for your program
+                print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+                print_array() - Given an array of data and a length, prints the array to the screen             [DONE]
+                find_median() - Given an array of data and a length, returns the median value                   [DONE]
+                find_mean() - Given an array of data and a length, returns the mean                             [DONE]
+                find_maximum() - Given an array of data and a length, returns the maximum                       [DONE]
+                find_minimum() - Given an array of data and a length, returns the minimum                       [DONE]
+                sort_array() - Given an array of data and a length, sorts the array from largest to smallest.   [DONE]
+                        (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
